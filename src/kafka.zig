f23e44d4f2d8e34c2121e1235559b57ce2df7c02
vspaz <vladimir.edelman@gmail.com>
2025-02-03 20:03:10 +0100

    simplify interfaces & imports

--- src/kafka.zig
+++ src/kafka.zig
@@ -1,14 +1,10 @@
 const std = @import("std");
-pub const config = @import("config.zig");
-pub const producer = @import("producer.zig");
-pub const consumer = @import("consumer.zig");
-pub const topic = @import("topic.zig");
-pub const utils = @import("utils.zig");
+pub const ConfigBuilder = @import("config.zig").Builder;
+pub const Producer = @import("producer.zig").Producer;
+pub const Consumer = @import("consumer.zig").Consumer;
+pub const TopicBuilder = @import("topic.zig").Builder;
+pub const toSlice = @import("utils.zig").toSlice;
 
 test {
     std.testing.refAllDecls(@This());
-}
-
-pub fn main() !void{
-
 }
