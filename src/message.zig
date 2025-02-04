ade6f7375865da72d781c4659be4b52e780658aa
vspaz <vladimir.edelman@gmail.com>
2025-02-04 21:56:40 +0100

    return []const u8 on null or payload == 0

--- src/message.zig
+++ src/message.zig
@@ -7,10 +7,10 @@ pub const Message = struct {
     const Self = @This();
     _message: *librdkafka.rd_kafka_message_t,
 
-    pub fn getPayload(self: Self) ?[]const u8 {
+    pub fn getPayload(self: Self) []const u8 {
         if (self._message.payload != null and self._message.len > 0) {
             return @as([*]u8, @ptrCast(self._message.payload))[0..self._message.len];
         }
-        return null;
+        return []const u8{};
     }
 };
